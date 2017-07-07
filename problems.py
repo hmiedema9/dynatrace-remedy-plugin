a7807e2c8cdaeb876d16dac54e149b0057288030
Hayden Miedema <haydenmiedema@gmail.com>
2017-07-06 19:39:43 -0700

    Adding correct file - wrong uploaded on first commit

---
+++ problems.py
@@ -0,0 +1,15 @@
+import requests
+import json
+
+response = requests.get('<URL+API_TOKEN>')
+jsonData = response.json()
+
+def test(data):
+    for key in data['result']:
+        if key == 'problems':
+            if not data['result'][key]:
+                print("no problems")
+            else:
+                print(data['result'][key])
+
+test(jsonData)
