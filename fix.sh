d3cfff17ce37828cd53549b409d67d25d02f8e4d
Hayden Miedema <haydenmiedema@gmail.com>
2017-07-20 23:42:20 -0700

    fix.sh

---
+++ fix.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+git filter-branch --env-filter '
+OLD_EMAIL="hayden_miedema@nationalnail.com"
+CORRECT_NAME="Hayden Miedema"
+CORRECT_EMAIL="haydenmiedema@gmail.com"
+if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_COMMITTER_NAME="$CORRECT_NAME"
+    export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
+fi
+if [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_AUTHOR_NAME="$CORRECT_NAME"
+    export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
+fi
+' --tag-name-filter cat -- --branches --tags
