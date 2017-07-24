976459bac1d78a71d66762bfe989c12266d6e6d4
Hayden Miedema <haydenmiedema@gmail.com>
2017-07-23 23:00:32 -0700

    using nodemailer for sending email on POST

--- server.js
+++ server.js
@@ -2,11 +2,32 @@ const express = require('express')
 const app = express()
 const port = 8080
 const bodyParser = require('body-parser');
-
+'use strict';
+const nodemailer = require('nodemailer');
 var router = express.Router();
 
 // for parsing JSON
 app.use(bodyParser.json({ type: 'application/json' }));
+
+// create reusable transporter object using the default SMTP transport
+let transporter = nodemailer.createTransport({
+    host: 'smtp.example.com',
+    port: 465,
+    secure: true, // secure:true for port 465, secure:false for port 587
+    auth: {
+        user: 'username@example.com',
+        pass: 'userpass'
+    }
+});
+
+// setup email data with unicode symbols
+let mailOptions = {
+    from: 'dynatrace-alerts@kp.org <dynatrace-alerts@kp.org>', // sender address
+    to: 'hayden.miedema@dynatrace.com, haydenmiedema@gmail.com', // list of receivers
+    subject: 'Test alert for problem notification', // Subject line
+    text: 'Test alert', // plain text body
+    html: '<b>Sending HTML in body</b>' // html body
+};
 
 app.use((request, response, next) => {  
   console.log(request.headers)
@@ -37,6 +58,14 @@ app.post('/', function(req, res) {
   res.json({
       message: 'received message'
    });
+
+   // send mail with defined transport object
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+        return console.log(error);
+    }
+    console.log('Message %s sent: %s', info.messageId, info.response);
+  });
  })
 
 app.listen(port, (err) => {  
